15f1f5b764a26f575bffe0e65f6238d8059bd22b
liwei <liwei@jclife.com>
2020-12-08 14:41:30 +0800

    第一次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="content">
+		<view @click="showAddress">
+			{{ this.places.length ? this.places.join('/') : '请选择地点'}}
+		</view>
+		<jc-address-picker 
+		:places='places' 
+		:visible.sync='visible'
+		@change='change'
+		></jc-address-picker>
+	</view>
+</template>
+
+<script>
+	import jcAddressPicker from '@/components/jc-address-picker/jc-address-picker.vue'
+	export default {
+		components: { jcAddressPicker },
+		data() {
+			return {
+				places: ['上海市','市辖区'],
+				visible: false
+			}
+		},
+		methods:{
+			change(data){
+				this.places = data.map(i=>i.name);
+			},
+			showAddress(){
+				this.visible = true;
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
